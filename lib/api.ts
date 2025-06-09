98b4edd55c45f7825b5310364a343526f2cc9587
biicaa4 <bahdree@bridge-i.asia>
2025-06-09 08:22:05 +0800

    add behaviour auto logout if token expired

--- lib/api.ts
+++ lib/api.ts
@@ -1,5 +1,10 @@
-import axios from "axios";
+import axios, { AxiosError, InternalAxiosRequestConfig } from "axios";
 import Cookies from "js-cookie";
+
+interface ApiError {
+  message: string;
+  success: boolean;
+}
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL ? `${process.env.NEXT_PUBLIC_API_URL}/api` : "http://localhost:3001/api";
 
@@ -7,13 +12,36 @@ const api = axios.create({
   baseURL: API_URL,
 });
 
-// Add token to requests
-api.interceptors.request.use((config) => {
-  const token = Cookies.get("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+// Request interceptor
+api.interceptors.request.use(
+  (config: InternalAxiosRequestConfig) => {
+    const token = Cookies.get("token");
+    if (token && config.headers) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error: AxiosError) => {
+    return Promise.reject(error);
   }
-  return config;
-});
+);
+
+// Response interceptor
+api.interceptors.response.use(
+  (response) => response,
+  (error: AxiosError<ApiError>) => {
+    if (error.response?.status === 401) {
+      // Clear auth data
+      Cookies.remove("token");
+      localStorage.removeItem("user");
+
+      // Redirect to login page
+      if (typeof window !== "undefined" && window.location.pathname !== "/login") {
+        window.location.href = "/login";
+      }
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default api;
